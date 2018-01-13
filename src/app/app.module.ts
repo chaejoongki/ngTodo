287f43997e19240ad26bf6d68f7578e834f276a1
chae joongki <guers@naver.com>
2018-01-13 09:55:24 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent
   ],
   imports: [
     BrowserModule
